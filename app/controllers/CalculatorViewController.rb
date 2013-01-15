182b75dd10cf55807438d52e2244da80f94edbd3
Ray Tran <ray@artran.co.uk>
2013-01-15 22:22:35 +0000

    Changed to dynamically create lables and output label variables.

--- app/controllers/CalculatorViewController.rb
+++ app/controllers/CalculatorViewController.rb
@@ -5,76 +5,41 @@ class CalculatorViewController < UIViewController
         self.view.backgroundColor = UIColor.whiteColor
         self.title = "Training pace calculator"
 
-        distanceLabel = UILabel.alloc.initWithFrame([[20, 20],[195, 21]])
-        distanceLabel.text = "Length of recent race:"
-        self.view.addSubview(distanceLabel)
+        inputLabels = ["Length of recent race:", "Time taken (h:m:s):", "Display paces in:"]
+        inputLabels.each_with_index do |text, index|
+            label = UILabel.alloc.initWithFrame([[20, index * 29 + 20],[195, 21]])
+            label.text = text
+            self.view.addSubview(label)
+        end
 
         # distanceField
         # distance unit switch
-
-        @durationLabel = UILabel.alloc.initWithFrame([[20, 49],[195, 21]])
-        @durationLabel.text = "Time taken (h:m:s):"
-        self.view.addSubview(@durationLabel)
 
         # hour field
         # minute field
         # second field
 
-        @unitLabel = UILabel.alloc.initWithFrame([[20, 78],[195, 21]])
-        @unitLabel.text = "Display paces in:"
-        self.view.addSubview(@unitLabel)
-
         # pace unit switch
 
         # calculate button 20, 107
 
-        @easyLabel = UILabel.alloc.initWithFrame([[20, 180],[195, 21]])
-        @easyLabel.text = "Easy run training pace:"
-        self.view.addSubview(@easyLabel )
+        outputLabels = ["Easy run training pace:", "Tempo run training pace:",
+                        "VO2-max training pace:", "Speed form training pace:",
+                        "Long run training pace:", "Yasso 800 training pace:"]
+        outputLabels.each_with_index do |text, index|
+            label = UILabel.alloc.initWithFrame([[20, index * 29 + 180],[195, 21]])
+            label.text = text
+            self.view.addSubview(label)
+        end
 
-        @easyValueLabel = UILabel.alloc.initWithFrame([[224, 180],[195, 21]])
-        @easyValueLabel.text = "00:00"
-        self.view.addSubview(@easyValueLabel )
-
-        @tempoLabel = UILabel.alloc.initWithFrame([[20, 209],[195, 21]])
-        @tempoLabel.text = "Tempo run training pace:"
-        self.view.addSubview(@tempoLabel)
-
-        @tempoValueLabel = UILabel.alloc.initWithFrame([[224, 209],[195, 21]])
-        @tempoValueLabel.text = "00:00"
-        self.view.addSubview(@tempoValueLabel )
-
-        @vo2Label = UILabel.alloc.initWithFrame([[20, 238],[195, 21]])
-        @vo2Label.text = "VO2-max training pace:"
-        self.view.addSubview(@vo2Label )
-
-        @vo2ValueLabel = UILabel.alloc.initWithFrame([[224, 238],[195, 21]])
-        @vo2ValueLabel.text = "00:00"
-        self.view.addSubview(@vo2ValueLabel )
-
-        @speedFormLabel = UILabel.alloc.initWithFrame([[20, 267],[195, 21]])
-        @speedFormLabel.text = "Speed form training pace:"
-        self.view.addSubview(@speedFormLabel )
-
-        @speedFormValueLabel = UILabel.alloc.initWithFrame([[224, 267],[195, 21]])
-        @speedFormValueLabel.text = "00:00"
-        self.view.addSubview(@speedFormValueLabel )
-
-        @longRunLabel = UILabel.alloc.initWithFrame([[20, 296],[195, 21]])
-        @longRunLabel.text = "Long run training pace:"
-        self.view.addSubview(@longRunLabel )
-
-        @longRunValueLabel = UILabel.alloc.initWithFrame([[224, 296],[195, 21]])
-        @longRunValueLabel.text = "00:00"
-        self.view.addSubview(@longRunValueLabel )
-
-        @yassoLabel = UILabel.alloc.initWithFrame([[20, 325],[195, 21]])
-        @yassoLabel.text = "Yasso 800 training pace:"
-        self.view.addSubview(@yassoLabel )
-
-        @yassoValueLabel = UILabel.alloc.initWithFrame([[224, 325],[195, 21]])
-        @yassoValueLabel.text = "00:00"
-        self.view.addSubview(@yassoValueLabel )
+        outputValueLabels = ["@easyValueLabel", "@tempoValueLabel", "@vo2ValueLabel",
+                             "@speedFormValueLabel", "@longRunValueLabel", "@yassoValueLabel"]
+        outputValueLabels.each_with_index do |labelName, index|
+            outputValueLabel = UILabel.alloc.initWithFrame([[224, index * 29 + 180],[195, 21]])
+            outputValueLabel.text = "00:00"
+            self.view.addSubview(outputValueLabel)
+            instance_variable_set(labelName, outputValueLabel)
+        end
 
         # explain button 20, 354
     end
