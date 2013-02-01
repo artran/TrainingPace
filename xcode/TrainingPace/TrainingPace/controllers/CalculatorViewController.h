e4f493daedcdff1e07fc8e60b389e18c4fefb6a1
Ray Tran <ray@artran.co.uk>
2013-02-01 21:41:02 +0000

    Controller stubs added

---
+++ xcode/TrainingPace/TrainingPace/controllers/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  TrainingPace
+//
+//  Created by Ray Tran on 01/02/2013.
+//  Copyright (c) 2013 Artran Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
