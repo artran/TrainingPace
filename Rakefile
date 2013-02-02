8d469a799b72f250958f02452a95f0afa1b100d8
Ray Tran <ray@artran.co.uk>
2013-02-02 20:22:54 +0000

    Support iOS 6.0 and over. Add explanation page.

--- Rakefile
+++ Rakefile
@@ -6,6 +6,9 @@ require 'motion-testflight'
 Motion::Project::App.setup do |app|
   # Use `rake config' to see complete project settings.
   app.name = 'TrainingPace'
+  app.deployment_target = '6.0'
+  app.identifier = 'uk.co.artran.TrainingPace'
+  app.interface_orientations = [:portrait]
   app.provisioning_profile = '/Users/ray/Library/MobileDevice/Provisioning Profiles/508B92E8-9459-4CEB-A612-9DFF47C20DC9.mobileprovision'
   app.testflight.sdk = 'vendor/TestFlight'
   app.testflight.api_token = '1cc94371a3ae411b71594e66aa55246c_ODE2NDM3MjAxMy0wMS0wOCAxNDozODo1My45OTk2Njc'
--- app/controllers/CalculatorViewController.rb
+++ app/controllers/CalculatorViewController.rb
@@ -1,5 +1,4 @@
 class CalculatorViewController < UIViewController
-
   # Outlets
   attr_accessor :distance_unit_choice
   attr_accessor :pace_unit_choice
@@ -19,11 +18,91 @@ class CalculatorViewController < UIViewController
       super
   end
   
+  # segue methods
+  def prepareForSegue(segue, sender:sender)
+    if !@web_view then
+      @web_view = UIWebView.alloc.init
+      path = NSBundle.mainBundle.pathForResource('explainPaceCalculator', ofType: 'html')
+      url = NSURL.fileURLWithPath(path)
+      @web_view.loadRequest NSURLRequest.requestWithURL(url)
+    end
+    
+    destination = segue.destinationViewController
+    destination.setTitle("Explain Pace Calculator")
+    destination.setView(@web_view)
+  end
+  
   # Actions
   def pace_unit_changed(sender)
+    self.metric_pace = self.pace_unit_choice.selectedSegmentIndex == 0
+    calculate_paces
   end
   
   def calculate_pressed(sender)
     self.easy_pace_label.text = '1:23'
   end
+  
+  ################ calculator implementation ################
+
+  def setup_calculation
+       #   self.minsTaken = [self.timeTakenHrsEditor.text floatValue] * 60
+       #      + [self.timeTakenMinsEditor.text floatValue]
+       #      + [self.timeTakenSecsEditor.text floatValue] /60;
+       #    
+       #      self.metresRaced = [self.distanceEditor.text floatValue];
+       #    
+       #      self.metricDistance = (self.distanceUnitChoice.selectedSegmentIndex == 0);
+       #    
+       #      if (self.metricDistance) {
+       #          self.metresRaced *= 1000;
+       #      } else {
+       #          self.metresRaced *= 1609;
+       #      }
+       #    
+       #      self.speed = self.metresRaced / self.minsTaken;
+       #    
+       #      self.vo2Max = [RunCalcMaths vo2ForVelocity:self.speed] / [RunCalcMaths percentVO2MaxForTime:self.minsTaken];
+       #    
+       #      [self calculatePaces];
+  end
+
+  def calculate_paces
+        # float velEasy = [RunCalcMaths velocityForVO2:(self.vo2Max * .7)];
+        # float velTempo = [RunCalcMaths velocityForVO2:(self.vo2Max * .88)];
+        # float velVo2 = [RunCalcMaths velocityForVO2:(self.vo2Max)];
+        # float velSpeed = [RunCalcMaths velocityForVO2:(self.vo2Max * 1.1)];
+        # float velLong = [RunCalcMaths velocityForVO2:(self.vo2Max * .6)];
+        # float velYasso = velVo2 * 1.95;  
+        #     
+        # NSString *toAppend;
+        # self.metricPace = (self.paceUnitChoice.selectedSegmentIndex == 0);
+        # if (self.metricPace) {
+        #     toAppend = @" min/km";
+        # } else {
+        #     toAppend = @" min/mile";
+        # }
+        #     
+        # self.easyPaceLabel.text = [NSString stringWithFormat:@"%@ %@", [self paceForSpeed:velEasy], toAppend];
+        # self.tempoPaceLabel.text = [NSString stringWithFormat:@"%@ %@", [self paceForSpeed:velTempo], toAppend];
+        # self.vo2PaceLabel.text = [NSString stringWithFormat:@"%@ %@", [self paceForSpeed:velVo2], toAppend];
+        # self.speedPaceLabel.text = [NSString stringWithFormat:@"%@ %@", [self paceForSpeed:velSpeed], toAppend];
+        # self.longPaceLabel.text = [NSString stringWithFormat:@"%@ %@", [self paceForSpeed:velLong], toAppend];
+        # 
+        # BOOL oldMetricPace = self.metricPace;
+        # self.metricPace = NO;
+        # self.yassoPaceLabel.text = [NSString stringWithFormat:@"%@ min/800", [self paceForSpeed:velYasso]];
+        # self.metricPace = oldMetricPace;
+    end
+
+  # Takes a speed in metres / minute a converts it to a string representing a pace in
+  # minutes per mile or km.   
+  def pace_for_speed(speed)
+      # float factor = (self.metricPace ? 1000 : 1609);
+      # speed = 1 / speed * factor;
+      #     
+      # int minutes = floor(speed);
+      # int seconds = floor((speed - minutes) * 60);
+      #     
+      # return [NSString stringWithFormat:@"%i : %@%i", minutes, (seconds > 9 ? @"" : @"0"), seconds];
+    end
 end
