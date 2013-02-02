1a60dde1e4419e048eff156e14b731699353c309
Ray Tran <ray@artran.co.uk>
2013-02-02 18:03:01 +0000

    First iteration of switching to storyboard complete.

--- Rakefile
+++ Rakefile
@@ -10,4 +10,5 @@ Motion::Project::App.setup do |app|
   app.testflight.sdk = 'vendor/TestFlight'
   app.testflight.api_token = '1cc94371a3ae411b71594e66aa55246c_ODE2NDM3MjAxMy0wMS0wOCAxNDozODo1My45OTk2Njc'
   app.testflight.team_token = '0a61d3da55ba5669e5933df957bb2d9f_MTczMTIwMjAxMy0wMS0xMiAxNzoxNjoyNy43NzYwNzg'
+  app.info_plist['UIMainStoryboardFile'] = 'MainStoryboard'
 end
--- app/controllers/CalculatorViewController.rb
+++ app/controllers/CalculatorViewController.rb
@@ -1,56 +1,29 @@
 class CalculatorViewController < UIViewController
-    def viewDidLoad
-        super
 
-        self.view.backgroundColor = UIColor.whiteColor
-        self.title = "Training pace calculator"
+  # Outlets
+  attr_accessor :distance_unit_choice
+  attr_accessor :pace_unit_choice
+  attr_accessor :distance_field
+  attr_accessor :time_hours_field
+  attr_accessor :time_mins_field
+  attr_accessor :time_secs_field
 
-        input_labels = ["Length of race:", "Time (h:m:s):", "Pace units:"]
-        input_labels.each_with_index do |text, index|
-            label = UILabel.alloc.initWithFrame([[20, index * 45 + 31],[120, 21]])
-            label.text = text
-            self.view.addSubview(label)
-        end
-
-        @distance_field = UITextField.alloc.initWithFrame([[142, 26], [40, 31]])
-        @distance_field.borderStyle = UITextBorderStyleRoundedRect
-        @distance_field.textAlignment = UITextAlignmentLeft
-        @distance_field.keyboardType = UIKeyboardTypeDecimalPad
-        self.view.addSubview(@distance_field)
-
-        # distance unit switch
-        #@pace_unit_switch = UISegmentedControl.alloc.initWithFrame([[190, 20], [110, 44]])
-        @pace_unit_switch = UISegmentedControl.alloc.initWithItems(["km", "miles"])
-        @pace_unit_switch.frame = [[190, 20], [110, 44]]
-        @pace_unit_switch.selectedSegmentIndex = 0
-        self.view.addSubview(@pace_unit_switch)
-
-        # hour field
-        # minute field
-        # second field
-
-        # pace unit switch
-
-        # calculate button 20, 107
-
-        output_labels = ["Easy run training pace:", "Tempo run training pace:",
-                        "VO2-max training pace:", "Speed form training pace:",
-                        "Long run training pace:", "Yasso 800 training pace:"]
-        output_labels.each_with_index do |text, index|
-            label = UILabel.alloc.initWithFrame([[20, index * 29 + 180],[195, 21]])
-            label.text = text
-            self.view.addSubview(label)
-        end
-
-        output_value_labels = ["@easyValueLabel", "@tempoValueLabel", "@vo2ValueLabel",
-                             "@speedFormValueLabel", "@longRunValueLabel", "@yassoValueLabel"]
-        output_value_labels.each_with_index do |label_name, index|
-            output_value_label = UILabel.alloc.initWithFrame([[224, index * 29 + 180],[195, 21]])
-            output_value_label.text = "00:00"
-            self.view.addSubview(output_value_label)
-            instance_variable_set(label_name, output_value_label)
-        end
-
-        # explain button 20, 354
-    end
+  attr_accessor :easy_pace_label
+  attr_accessor :tempo_pace_label
+  attr_accessor :vo2_pace_label
+  attr_accessor :speed_pace_label
+  attr_accessor :long_pace_label
+  
+  # iOS lifecycle methods
+  def viewDidLoad
+      super
+  end
+  
+  # Actions
+  def pace_unit_changed(sender)
+  end
+  
+  def calculate_pressed(sender)
+    self.easy_pace_label.text = '1:23'
+  end
 end
